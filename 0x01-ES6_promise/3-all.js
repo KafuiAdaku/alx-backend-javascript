356b45aaa853640e55368274cff3d01366f5667e
Dennis Adaku <dennisadaku@ymail.com>
2024-01-03 18:42:16 +0000

    refactored 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((result) => {
       const [uploadPhoto, user] = result;
       console.log(`${uploadPhoto.body} ${user.firstName} ${user.lastName}`);
