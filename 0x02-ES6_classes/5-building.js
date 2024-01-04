5bed18ab511be334bcc1ff03a90c619f140d3dd9
Dennis Adaku <dennisadaku@ymail.com>
2024-01-04 17:12:30 +0000

    modified solution to task 5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,12 @@
 export default class Building {
   constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this._sqft = sqft;
   }
 
   get sqft() {
     return this._sqft;
-  }
-
-  set sqft(sqft) {
-    this._sqft = sqft;
-  }
-
-  /* eslint-disable class-methods-use-this */
-  evacuationWarningMessage() {
-    throw new Error('Class extending Building must override evacuationWarningMessage');
   }
 }
