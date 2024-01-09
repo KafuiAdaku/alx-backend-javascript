66ec7a85fb8ecf81207375d5f3169956c9fd98dd
Dennis Adaku <dennisadaku@ymail.com>
2024-01-09 16:51:30 +0000

    modified solution to task 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,8 +4,8 @@ export default function createInt8TypedArray(length, position, value) {
   }
 
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const dataView = new DataView(buffer);
+  dataView.setInt8(position, value);
 
-  int8Array[position] = value;
   return buffer;
 }
