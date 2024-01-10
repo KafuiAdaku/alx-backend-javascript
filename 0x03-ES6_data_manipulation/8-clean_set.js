dad236becc9e051270fa12158a483522e4bee78c
Dennis Adaku <dennisadaku@ymail.com>
2024-01-10 11:27:23 +0000

    modified solution to task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,19 @@
 export default function cleanSet(set, startString) {
-  const stringArr = [];
+//  const stringArr = [];
   if (startString === undefined || startString === '') {
     return '';
   }
+
+  return [...set]
+    .filter((str) => str.startsWith(startString))
+    .map((str) => str.slice(startString.length))
+    .join('-');
+/*
   set.forEach((string) => {
     if (string.startsWith(startString)) {
       stringArr.push(string.slice(startString.length));
     }
   });
   return stringArr.join('-');
+*/
 }
