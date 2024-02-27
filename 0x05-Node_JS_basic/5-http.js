e5e0d79299649e61913200dd82156d5802e76cd5
Dennis Adaku <117731916+KafuiAdaku@users.noreply.github.com>
2024-02-27 17:21:34 +0000

    added solution totask 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,57 @@
+// const countStudents = require('./3-read_file_async');
+const fs = require('fs');
+const http = require('http');
+function countStudents(path) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(path, 'utf8', (err, data) => {
+            if (err) {
+                reject(new Error('Cannot load the database'));
+                return;
+            }
+            const lines = data.split('\n').filter((line) => line.length > 0);
+            const students = lines.slice(1); // Exclude header
+
+            const numStudents = `Number of students: ${students.length}`;
+
+            const fields = {};
+            for (const student of students) {
+                const [firstname, , , field] = student.split(',');
+                if (!fields[field]) {
+                    fields[field] = [];
+                }
+                fields[field].push(firstname);
+            }
+
+            let output = ``;
+            for (const [field, students] of Object.entries(fields)) {
+                output += `Number of students in ${field}: ${students.length}. List: ${students.join(', ')}\n`;
+            }
+            resolve(output);
+        })
+    })
+}
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  }
+  if (req.url === '/students') {
+    countStudents('database.csv')
+      .then((data) => {
+        res.write('This is the list of our students\n');
+        res.end(data);
+      })
+      .catch((error) => {
+        res.end(error.message);
+      });
+  }
+});
+
+app.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}`);
+});
+module.exports = app;
