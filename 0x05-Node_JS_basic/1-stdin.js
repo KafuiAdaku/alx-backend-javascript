63f9c996584d4445ba3d64e63ab818724c198721
Dennis Adaku <117731916+KafuiAdaku@users.noreply.github.com>
2024-02-27 08:02:10 +0000

    modified solution to task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,4 +1,4 @@
-process.stdout.write("Welcome to holberton School, what is your name?\n")
+process.stdout.write("Welcome to Holberton School, what is your name?")
 
 process.stdin.on("readable", () => {
     const name = process.stdin.read()
