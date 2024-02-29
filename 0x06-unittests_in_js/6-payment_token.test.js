f3be44a5fe102873e120fe14be51dfd19d6a7bfc
Dennis Adaku <117731916+KafuiAdaku@users.noreply.github.com>
2024-02-29 21:14:44 +0000

    fix: modified solutionto task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
 describe('getPaymentTokenFromAPI', function() {
-    it('should resolve with correct data when input is true', function() {
+    it('should resolve with correct data when input is true', function(done) {
         getPaymentTokenFromAPI(true)
             .then((data) => {
                 expect(data).to.eql({data: 'Successful response from the API'});
