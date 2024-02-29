3aaf05db4fe51ab51c1872f6b4fc8b7368435c38
Dennis Adaku <117731916+KafuiAdaku@users.noreply.github.com>
2024-02-29 20:52:15 +0000

    added solution to task 5

---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,27 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const expect = chai.expect;
+const sendPaymentRequestToApi = require('./5-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', function() {
+    let consoleLogSpy;
+
+    beforeEach(function() {
+        consoleLogSpy = sinon.spy(console, 'log');
+    });
+
+    afterEach(function() {
+        consoleLogSpy.restore();
+    });
+
+    it('should log "The total is: 120" for inputs 100 and 20', function() {
+        sendPaymentRequestToApi(100, 20);
+        expect(consoleLogSpy.calledOnceWith('The total is: 120')).to.be.true;
+    });
+
+    it('should log "The total is: 20" for inputs 10 and 10', function() {
+        sendPaymentRequestToApi(10, 10);
+        expect(consoleLogSpy.calledOnceWith('The total is: 20')).to.be.true;
+    });
+});
